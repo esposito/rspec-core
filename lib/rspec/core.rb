7715af0dfd3bdc79134a962f02f754e4f35826fb
Jon Rowe <hello@jonrowe.co.uk>
2015-02-08 15:49:04 +1100

    remove usage of set from rspec-core and replace with an internal lookupset which uses a hash

--- lib/rspec/core.rb
+++ lib/rspec/core.rb
@@ -9,6 +9,46 @@ RSpec::Support.require_rspec_support "caller_filter"
 
 RSpec::Support.define_optimized_require_for_rspec(:core) { |f| require_relative f }
 
+module RSpec
+  module Core
+    # @private
+    class LookupSet
+      include Enumerable
+
+      def initialize(array=[])
+        @values = {}
+        merge(array)
+      end
+
+      def <<(key)
+        @values[key] = true
+      end
+
+      def each(&block)
+        @values.keys.each(&block)
+      end
+
+      def to_a
+        @values.keys
+      end
+
+      def +(others)
+        LookupSet.new(@values).merge(others)
+      end
+
+      def include?(key)
+        @values[key] == true
+      end
+
+      def merge(values)
+        values.each do |key|
+          @values[key] = true
+        end
+      end
+    end
+  end
+end
+
 %w[
   version
   warnings
